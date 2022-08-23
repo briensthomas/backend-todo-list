fc0cf84d314ed1e95607b9a4209a831e1d979c30
Brien Thomas <thomasbrien94@gmail.com>
2022-08-23 15:13:17 -0700

    test passes on /sessions route, logs in a user and tests for the message response

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -11,21 +11,21 @@ const mockUser = {
   password: '123456',
 };
 
-const registerAndLogin = async (userProps = {}) => {
-  const password = userProps.password ?? mockUser.password;
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
 
-  // Create an "agent" that gives us the ability
-  // to store cookies between requests in a test
-  const agent = request.agent(app);
+//   // Create an "agent" that gives us the ability
+//   // to store cookies between requests in a test
+//   const agent = request.agent(app);
 
-  // Create a user to sign in with
-  const user = await UserService.create({ ...mockUser, ...userProps });
+//   // Create a user to sign in with
+//   const user = await UserService.create({ ...mockUser, ...userProps });
 
-  // ...then sign in
-  const { email } = user;
-  await agent.post('/api/v1/users/sessions').send({ email, password });
-  return [agent, user];
-};
+//   // ...then sign in
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -36,7 +36,7 @@ describe('backend-express-template routes', () => {
   });
 
   it('#POST /api/v1/users creates a new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request.agent(app).post('/api/v1/users').send(mockUser);
     const { firstName, lastName, email } = mockUser;
 
     console.log('res.body', res.body);
@@ -49,16 +49,14 @@ describe('backend-express-template routes', () => {
   });
 
   it('#POST /api/v1/users/sessions should log in an existing user', async () => {
-    const agent = await registerAndLogin();
+    const agent = request.agent(app);
+    await agent.post('/api/v1/users').send(mockUser);
     const res = await agent.post('/api/v1/users/sessions').send(mockUser);
-    const { firstName, lastName, email } = mockUser;
 
+    console.log('res.body', res.body);
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
-      id: expect.any(String),
-      firstName,
-      lastName,
-      email 
+      message: 'You have successfully signed in!'
     });
   });
 
