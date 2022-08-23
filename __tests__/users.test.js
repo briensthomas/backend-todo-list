78dc25b0f9bccf282cbdb8e6411ec0ec8e193155
Brien Thomas <thomasbrien94@gmail.com>
2022-08-23 11:02:35 -0700

    test for POST to users successfully creates a new user, passes

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const UserService = require('../lib/services/UserServices');
 
 const mockUser = {
   firstName: 'Test',
@@ -10,24 +11,21 @@ const mockUser = {
   password: '123456',
 };
 
-const registerAndLogin = async (userProps = {}) => {
-  const password = userProps.password ?? mockUser.password;
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
 
-  // Create an "agent" that gives us the ability
-  // to store cookies between requests in a test
-  const agent = request.agent(app);
+//   // Create an "agent" that gives us the ability
+//   // to store cookies between requests in a test
+//   const agent = request.agent(app);
 
-  // Create a user to sign in with
-  // const user = await UserService.create({ ...mockUser, ...userProps });
+//   // Create a user to sign in with
+//   const user = await UserService.create({ ...mockUser, ...userProps });
 
-  // ...then sign in
-  // const { email } = user;
-  const resp = await agent
-    .post('/api/v1/users')
-    .send({ ...mockUser, ...userProps });
-  const user = resp.body;
-  return [agent, user];
-};
+//   // ...then sign in
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -40,7 +38,8 @@ describe('backend-express-template routes', () => {
   it('#POST /api/v1/users creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     const { firstName, lastName, email } = mockUser;
-
+    
+    console.log('res.body', res.body);
     expect(res.body).toEqual({
       id: expect.any(String),
       firstName,
