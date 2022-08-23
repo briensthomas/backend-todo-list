7eb8a79d09dc011662a28cfb79b772091627d401
Brien Thomas <thomasbrien94@gmail.com>
2022-08-23 09:52:26 -0700

    imported cors, setup app.js to use the front-end localhost address, and cookieParser. First test written for a POST to /users, expects to see a user created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,54 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  // const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  // const { email } = user;
+  const resp = await agent
+    .post('/api/v1/users')
+    .send({ ...mockUser, ...userProps });
+  const user = resp.body;
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
   afterAll(() => {
     pool.end();
   });
+
+  it('#POST /api/v1/users creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+
+  
+
 });
